98c0e4b5280e5a8d044c8bc85464c6ac8cc2dcb3
liaopujian <liaopj@fshows.com>
2019-12-09 22:01:38 +0800

    项目初始化

---
+++ backend/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "github.com/kataras/iris"
+
+func main(){
+	//1.创建iris实例
+	app := iris.New()
+	//2.设置错误模式，在mvc模式下提示错误
+	app.Logger().SetLevel("debug")
+	//3.注册模板目录和布局文件
+	tmp := iris.HTML("./backend/web/views", ".html").
+		Layout("shared/layout.html").Reload(true)
+	//注册
+	app.RegisterView(tmp)
+	//4.设置模板目录
+
+	//出现问题跳转到指定页面
+	app.OnAnyErrorCode(func(ctx iris.Context) {
+		ctx.ViewData("message", ctx.Values().GetStringDefault("message", "访问的页面出错！"))
+		ctx.ViewLayout("")
+		_ := ctx.View("shared/error.html")
+	})
+
+	//5.注册控制器
+
+	//6.启动服务
+	_ := app.Run(
+		iris.Addr("localhost:8080"),
+		iris.WithoutServerError(iris.ErrServerClosed),
+		iris.WithOptimizations,
+		)
+
+}
